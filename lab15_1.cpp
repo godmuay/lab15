b9124945c14e32a988754c1737d66f78a1be83c1
muay <apinyakasi19@gmail>
2023-01-17 15:53:15 +0700

    15.1-F

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	T x;
+	for(int i = 0; i<N-1 ; i++){
+		for(int j=i+1;j>0;j--){
+			if(d[j]>d[j-1]){
+				x=d[j-1];
+				d[j-1]=d[j];
+				d[j]=x;
+			}
+		}
+		cout << "Pass " << i+1 << ":";
+		for(int l=0; l<N;l++){
+			cout << d[l] << " ";
+		}
+		cout << endl;
+	}
 
 }
 
